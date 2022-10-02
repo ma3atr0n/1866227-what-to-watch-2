d38459954939584e79a705f6638a6a9f765b3583
ma3atr0n <38848459+ma3atr0n@users.noreply.github.com>
2022-10-02 13:18:55 +0300

    5.9. Завершаем проектировать базу данных

--- src/app/application.ts
+++ src/app/application.ts
@@ -39,15 +39,6 @@ export default class Application {
 
     await this.DBClient.connect(uri);
 
-    // const comment = {
-    //   comment: '123554322asdf',
-    //   rating: 5,
-    //   date: '2020-01-02',
-    //   userId: '6330a6be9eecf0bb0c9a7672'
-    // };
-
-    // const result = await this.commentService.create('6330a6be9eecf0bb0c9a7675', comment);
-    //const result = await this.filmService.findDetails('6330a6be9eecf0bb0c9a7675');
     const result = await this.commentService.find('6330a6be9eecf0bb0c9a7675');
     console.log(result);
   }
